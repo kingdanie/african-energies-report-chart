37be59349e49fb29518df4e15770b44efc552e51
Daniel <52611355+kingdanie@users.noreply.github.com>
2025-11-23 14:04:27 +0100

    removed unnecessary components

--- src/admin/routes.jsx
+++ src/admin/routes.jsx
@@ -1,11 +1,7 @@
 import { createHashRouter } from "react-router-dom";
 import ApplicationLayout from "../components/application-layout/LayoutOne";
-import Settings from "./pages/settings";
 import ErrorPage from "./pages/error/Error";
-import Inbox from "./pages/inbox";
 import Dashboard from "./pages/dashboard";
-import LoginPage from "./pages/login";
-import Charts from "./pages/charts";
 import Commodities from "./pages/commodities";
 import BasketPrices from "./pages/basket-prices";
 import Countries from "./pages/countries";
@@ -23,23 +19,6 @@ export const router = createHashRouter([
       {
         path: "dashboard",
         element: <Dashboard />,
-      },
-      {
-        path: "inbox",
-        element: <Inbox />,
-      },
-     
-      {
-        path: "settings",
-        element: <Settings />,
-      },
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-      {
-        path: "charts",
-        element: <Charts />,
       },
       {
         path: "commodities",
